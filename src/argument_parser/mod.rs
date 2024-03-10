5e371b15deb3df54080d52901ec57c90dd56ca21
Genzo <genzo.namikawa@outlook.com>
2024-03-11 00:52:57 +0900

    refactor: separate build function from command list

--- src/argument_parser/mod.rs
+++ src/argument_parser/mod.rs
@@ -18,19 +18,19 @@ enum Commands {
 pub fn parse_cli_arguments() {
     let args = Args::parse();
 
-    match &args.commands {
-        Some(Commands::Build { path: path_str }) => {
-            let path = path::Path::new(path_str);
-
-            if !path_str.is_empty() && path.is_file() {
-                println!("Building project at '{}'", path_str);
-                compiler::compile(path_str).unwrap_or_default();
-            } else {
-                println!("Invalid path was specified. '{}'", path_str);
-            }
-        }
+    match args.commands {
+        Some(Commands::Build{path}) => build(path),
         None => {
             println!("No arguments specified. Try --help or -h for available commands.");
         }
+    }
+}
+
+fn build(path_str: String) {
+    if !path_str.is_empty() && path::Path::new(&path_str).is_file() {
+        println!("Building project at '{}'", &path_str);
+        compiler::compile(&path_str).unwrap_or_default();
+    } else {
+        println!("Invalid path was specified. '{}'", &path_str);
     }
 }
